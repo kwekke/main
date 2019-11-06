86f40a82483c59ffeaa6c8f114e8fd6f809fffae
kwekke <kwekkeeen@gmail.com>
2019-11-07 05:24:47 +0800

    Fix suggest command invalid command message

--- src/main/java/seedu/exercise/logic/commands/SuggestCommand.java
+++ src/main/java/seedu/exercise/logic/commands/SuggestCommand.java
@@ -12,8 +12,8 @@ public abstract class SuggestCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all suggested exercises";
 
-    public static final String MESSAGE_USAGE =
-            "BASIC: " + MESSAGE_USAGE_SUGGEST_BASIC + "\n"
+    public static final String MESSAGE_USAGE = "Suggest: suggests exercises. \n"
+            + "BASIC: " + MESSAGE_USAGE_SUGGEST_BASIC + "\n"
             + "POSSIBLE: " + MESSAGE_USAGE_SUGGEST_POSSIBLE;
 
 }
